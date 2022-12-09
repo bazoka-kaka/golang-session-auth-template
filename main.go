475ac58df365bfce39c7910366c4ee1286b1ac8d
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-12-09 17:34:43 +0700

    add main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"session-auth/controllers"
+	"session-auth/middlewares"
+)
+
+func main() {
+	// show pages
+	http.Handle("/", middlewares.RequireGET(http.HandlerFunc(controllers.ShowPage)))
+	http.Handle("/admin", middlewares.RequireGET(http.HandlerFunc(controllers.ShowPage)))
+	http.Handle("/register", middlewares.RequireGET(http.HandlerFunc(controllers.ShowPage)))
+	http.Handle("/login", middlewares.RequireGET(http.HandlerFunc(controllers.ShowPage)))
+
+	// handlers
+	http.Handle("/user/register", middlewares.RequirePOST(http.HandlerFunc(controllers.HandleRegister)))
+	http.Handle("/user/login", middlewares.RequirePOST(http.HandlerFunc(controllers.HandleLogin)))
+
+	fmt.Println("server running on port 3000")
+	http.ListenAndServe(":3000", nil)
+}
